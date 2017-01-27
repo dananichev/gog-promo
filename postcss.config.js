1ed91e31d614e5a6d1188367ef6ec24279fa53b8
Dmitry Ananichev <dananichev@360lab.com>
2017-01-27 17:36:29 +0500

    basic dev env with server added (via webpack)

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-font-magician'),
+    require('postcss-url'),
+    require('postcss-cssnext'),
+    require('postcss-clean'),
+    require('postcss-reporter'),
+  ],
+};
