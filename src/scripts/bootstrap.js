da667c7e2e484dcc51a087e9846e5b88a403d469
Dmitry Ananichev <dananichev@360lab.com>
2017-01-30 04:18:16 +0500

    GOG-PROMO-2 code style issues fix

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -6,31 +6,31 @@ const fill = document.querySelector('.range-input-fill');
 const valueWrapper = document.querySelector('.range-value');
 const gameItems = document.querySelectorAll('.games-bundle-item');
 
-const minValue = 0.99;
+const minValue = 0.99; //eslint-disable-line
 const maxValue = 49.99;
 const averageValue = 7.67;
 const topValue = 18.31;
 const rangeWidth = range.offsetWidth;
 
-range.addEventListener('input', e => {
+range.addEventListener('input', (e) => {
   const value = parseFloat(e.target.value);
   const percent = (value * 100) / maxValue;
   inputValue.value = value;
-  fill.style.width = `${rangeWidth * (percent / 100) - 15}px`;
-  valueWrapper.style.left = `${rangeWidth * (percent / 100) - 15}px`;
-  
+  fill.style.width = `${(rangeWidth * (percent / 100)) - 15}px`;
+  valueWrapper.style.left = `${(rangeWidth * (percent / 100)) - 15}px`;
+
   if (value > averageValue) {
     gameItems[1].classList.add('active');
   }
-  
+
   if (value > topValue) {
     gameItems[2].classList.add('active');
   }
-  
+
   if (value < averageValue) {
     gameItems[1].classList.remove('active');
   }
-  
+
   if (value < topValue) {
     gameItems[2].classList.remove('active');
   }
