5e48251ff6d59ce744e8c21810fa0a4ddae93bd7
Dmitry Ananichev <dananichev@360lab.com>
2017-01-30 04:09:29 +0500

    initial commit for slider

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -1,1 +1,37 @@
 import '../styles/bundle.css';
+
+const range = document.querySelector('#promo-range');
+const inputValue = document.querySelector('#promo-value');
+const fill = document.querySelector('.range-input-fill');
+const valueWrapper = document.querySelector('.range-value');
+const gameItems = document.querySelectorAll('.games-bundle-item');
+
+const minValue = 0.99;
+const maxValue = 49.99;
+const averageValue = 7.67;
+const topValue = 18.31;
+const rangeWidth = range.offsetWidth;
+
+range.addEventListener('input', e => {
+  const value = parseFloat(e.target.value);
+  const percent = (value * 100) / maxValue;
+  inputValue.value = value;
+  fill.style.width = `${rangeWidth * (percent / 100) - 15}px`;
+  valueWrapper.style.left = `${rangeWidth * (percent / 100) - 15}px`;
+  
+  if (value > averageValue) {
+    gameItems[1].classList.add('active');
+  }
+  
+  if (value > topValue) {
+    gameItems[2].classList.add('active');
+  }
+  
+  if (value < averageValue) {
+    gameItems[1].classList.remove('active');
+  }
+  
+  if (value < topValue) {
+    gameItems[2].classList.remove('active');
+  }
+});
